c73f2e6c4eb2b463e9ee15a7cdf479b46748b8f4
eilyin <evgen.ilyin@gmail.com>
2020-03-18 17:20:15 +0300

    Add Project 05. World Scramble

---
+++ Project 05. World Scramble/Project 05. World Scramble/ViewController.swift
@@ -0,0 +1,153 @@
+//
+//  ViewController.swift
+//  Project 05. World Scramble
+//
+//  Created by Eugene Ilyin on 02/09/2019.
+//  Copyright © 2019 Eugene Ilyin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    // MARK: - Properties
+    var savedWord = String()
+    var savedEntries = [String]()
+    
+    // Mark: Properties
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    // Mark: Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
+        
+        let defaults = UserDefaults.standard
+        if let savedWord = defaults.string(forKey: "SavedWord") {
+            title = savedWord
+        }
+        if let savedEntries = defaults.object(forKey: "SavedEntries") as? [String] {
+            usedWords = savedEntries
+        }
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        if usedWords.count == 0 {
+            startGame()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+    
+    @objc func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        if word.count < 3 { return false }
+        
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        if lowerAnswer == title {
+            showErrorMessage(errorMessage: "You can't use the word from the title", title: "Cheetah!")
+            return
+        }
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(lowerAnswer, at: 0)
+                    save()
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                } else {
+                    showErrorMessage(errorMessage: "You can't just make them up, you know!", title: "Word not recognised")
+                }
+            } else {
+                showErrorMessage(errorMessage: "Be more original!", title: "Word used already")
+            }
+        } else {
+            guard let title = title?.lowercased() else { return }
+            showErrorMessage(errorMessage: "You can't spell that word from \(title)", title: "Word not possible")
+        }
+    }
+    
+    func showErrorMessage(errorMessage: String, title: String) {
+        let ac = UIAlertController(title: title, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func save() {
+        let defautls = UserDefaults.standard
+        defautls.set(title, forKey: "SavedWord")
+        defautls.set(usedWords, forKey: "SavedEntries")
+    }
+    
+}
+
