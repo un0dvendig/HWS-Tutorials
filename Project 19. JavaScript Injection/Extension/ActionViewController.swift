b15a6f666d44a87048efd377968500dd894ad4c6
eilyin <evgen.ilyin@gmail.com>
2020-03-18 17:54:34 +0300

    Add Project 19. JavaScript Injection

---
+++ Project 19. JavaScript Injection/Extension/ActionViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ActionViewController.swift
+//  Extension. Project 19. JavaScript Injection
+//
+//  Created by Eugene Ilyin on 23.11.2019.
+//  Copyright © 2019 Eugene Ilyin. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var script: UITextView!
+    
+    var pageTitle = ""
+    var pageURL = ""
+    let defaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(selectPreWrittenScript))
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
+                    guard let itemDictionary = dict as? NSDictionary else { return }
+                    guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+
+                    self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+                    self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+                    
+                    DispatchQueue.main.async {
+                        self?.title = self?.pageTitle
+                        
+                        let savedinfo = self?.defaults.string(forKey: self?.pageURL ?? "")
+                        if savedinfo != nil {
+                            self?.script.text = savedinfo
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+
+    @IBAction func done() {
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": script.text]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+        extensionContext?.completeRequest(returningItems: [item])
+        
+        defaults.set(script.text, forKey: pageURL)
+        
+    }
+    
+    @objc
+    func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            script.contentInset = .zero
+        } else {
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        script.scrollIndicatorInsets = script.contentInset
+        
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
+    }
+    
+    @objc
+    func selectPreWrittenScript() {
+        let ac = UIAlertController(title: "Select prewritten script", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Show document title", style: .default, handler: { (_) in
+            self.script.text = "alert(document.title);"
+        }))
+        ac.addAction(UIAlertAction(title: "Show document url", style: .default, handler: { (_) in
+            self.script.text = "alert(document.URL);"
+        }))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+
+}
---
+++ Project 19. JavaScript Injection/Project 19. JavaScript Injection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 19. JavaScript Injection
+//
+//  Created by Eugene Ilyin on 23.11.2019.
+//  Copyright © 2019 Eugene Ilyin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
