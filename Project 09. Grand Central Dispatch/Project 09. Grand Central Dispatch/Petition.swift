50a1eb3945000c9160a463e7793da05a476b57a0
eilyin <evgen.ilyin@gmail.com>
2020-03-18 17:21:42 +0300

    Add Project 09. Grand Central Dispatch

---
+++ Project 09. Grand Central Dispatch/Project 09. Grand Central Dispatch/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project 09. Grand Central Dispatch
+//
+//  Created by Eugene Ilyin on 02/09/2019.
+//  Copyright © 2019 Eugene Ilyin. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project 09. Grand Central Dispatch/Project 09. Grand Central Dispatch/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project 09. Grand Central Dispatch
+//
+//  Created by Eugene Ilyin on 02/09/2019.
+//  Copyright © 2019 Eugene Ilyin. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
